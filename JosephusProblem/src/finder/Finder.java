bbbdccba9a8518d5435decb4f33fdf4da5c86456
Jasper Zhang <dilong.zhang@hotmail.com>
2021-05-22 00:32:46 +0200

    Update Finder.java

--- JosephusProblem/src/finder/Finder.java
+++ JosephusProblem/src/finder/Finder.java
@@ -7,7 +7,7 @@ public class Finder {
         for (int i = 1;group.size()>1; i++){
             System.out.println(group);
             if (i > group.size()-1){
-                i -= (group.size()+1);
+                i -= (group.size());
             }
             System.out.println(group.get(i)+" wird entfernt.");
             System.out.println();
